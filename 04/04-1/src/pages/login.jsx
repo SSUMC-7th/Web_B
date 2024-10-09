d5dd4c232d3fb4512418700e0302fc62cb29589b
mlnls <tmdals140@naver.com>
2024-10-09 14:15:58 +0900

    File 수정

---
+++ 04/04-1/src/pages/login.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const login = () => {
+    return(
+        <>
+            <LoginText>로그인 페이지</LoginText>
+        </>
+    );
+}
+
+export default login;
+
+const LoginText = styled.h1`
+    color: white;
+`;
